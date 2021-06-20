bd60f0c4011ed6390d34310dc3c7875e47304015
Shubham Tawde <shubhamtawde.tawde@gmail.com>
2021-06-20 22:14:45 +0530

    Added method to test whether API is working or not

--- src/main/java/com/library/service/LibraryService.java
+++ src/main/java/com/library/service/LibraryService.java
@@ -2,14 +2,21 @@
  * 
  */
 package com.library.service;
-
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 /**
  * @author Shubham Tawde
  *
  */
 
 //the Class which will be exposed as a REST Service
+@Path("/library")
 public class LibraryService 
 {
-
+	@GET
+	@Path("test")
+	public String testMethod()
+	{
+		return "API is working as expected";
+	}
 }

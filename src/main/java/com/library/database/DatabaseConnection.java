49e55a7926c1a073f0a024e47a4d3f560582ca62
Shubham Tawde <shubhamtawde.tawde@gmail.com>
2021-06-22 00:19:50 +0530

    Basic DB Connection Logic to MySQL

--- src/main/java/com/library/database/DatabaseConnection.java
+++ src/main/java/com/library/database/DatabaseConnection.java
@@ -3,11 +3,38 @@
  */
 package com.library.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author Shubham Tawde
  * 
  */
 public class DatabaseConnection 
 {
+	/* The DB URL, Username, Password variables
+	 */
+	static final String dbURL = "jdbc:mysql://localhost:3306/library";
+	static final String dbUser = "root";
+	static final String dbPass = "12345";
 	
+	public Connection getConnection() throws SQLException
+	{
+		Connection dbConnection;
+		//instantiate the connection
+		try 
+		{
+			dbConnection = DriverManager.getConnection(dbURL, dbUser, dbPass);
+		}
+		catch (SQLException sqlExp) 
+		{
+			System.out.println("Error Description: " + sqlExp.getMessage());
+			sqlExp.printStackTrace();
+			throw sqlExp;
+		}
+		//return the database connection to the calling class
+		return dbConnection;
+	}
 }
+

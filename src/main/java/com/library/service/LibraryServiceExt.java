403bbb1f2a9642faf19be32b94be35bf6589cb95
Shubham Tawde <shubhamtawde.tawde@gmail.com>
2021-06-25 23:04:35 +0530

    Added Get Books Method:

--- src/main/java/com/library/service/LibraryServiceExt.java
+++ src/main/java/com/library/service/LibraryServiceExt.java
@@ -1,13 +1,37 @@
 package com.library.service;
 
+import java.sql.SQLException;
+
+import com.library.constants.BookErrorConstants;
 import com.library.dto.BookResponse;
+import com.library.service.impl.LibraryServiceImpl;
 
 public class LibraryServiceExt 
 {
 
-	public BookResponse getBooks() 
+	public BookResponse getBooks() throws SQLException 
 	{
-		return null;
+		BookResponse bookResponse = new BookResponse();
+		BookErrorConstants errorConstants = new BookErrorConstants();
+		LibraryServiceImpl libraryServiceImpl = new LibraryServiceImpl();
+		try
+		{
+			bookResponse = libraryServiceImpl.getBooks();
+		}
+		catch(SQLException sqlExp)
+		{
+			sqlExp.printStackTrace();
+			bookResponse.setErrorCode(errorConstants.SQL_EXP_ERROR_CODE);
+			bookResponse.setErrorMessage("SQL Exception in Class: " + getClass() + "\nCaused By: " + sqlExp.getMessage());
+			throw sqlExp;
+		}
+		catch(Exception exp)
+		{
+			exp.printStackTrace();
+			bookResponse.setErrorCode(errorConstants.GENERIC_EXP_ERROR_CODE);
+			bookResponse.setErrorMessage("SQL Exception in Class: " + getClass() + "\nCaused By: " + exp.getMessage());
+		}
+		return bookResponse;
 	}
 
 }

1c8f012e9609cd56c91742456f51b4d481f75c48
Shubham Tawde <shubhamtawde.tawde@gmail.com>
2021-06-25 21:03:04 +0530

    Added DTO variables

--- src/main/java/com/library/dto/BookInfo.java
+++ src/main/java/com/library/dto/BookInfo.java
@@ -1,6 +1,44 @@
 package com.library.dto;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class BookInfo 
 {
-
+	private int bookId;
+	private String bookTitle;
+	private String bookAuthor;
+	private int bookQty;
+	
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+	public int getBookQty() {
+		return bookQty;
+	}
+	public void setBookQty(int bookQty) {
+		this.bookQty = bookQty;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book Id:" + this.bookId + " Book Title:" + this.bookTitle + "\t \t" + " Book Author:" + this.bookAuthor + "\t \t"  + " Book Quantity:" + this.bookQty;
+	}
+	
+	
 }

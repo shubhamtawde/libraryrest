c6f16182c4bc78e1f1f4359e1040a1932568971a
Shubham Tawde <shubhamtawde.tawde@gmail.com>
2021-06-26 16:18:59 +0530

    Added constants for validation

--- src/main/java/com/library/constants/BookErrorConstants.java
+++ src/main/java/com/library/constants/BookErrorConstants.java
@@ -5,4 +5,8 @@ public class BookErrorConstants
 	public static final int SQL_EXP_ERROR_CODE = -1;
 	
 	public static final int GENERIC_EXP_ERROR_CODE = -2;
+	
+	public static final int NO_CHAR_PRESENT = -3;
+	
+	public static final int IS_EMPTY_FIELD = -4;
 }

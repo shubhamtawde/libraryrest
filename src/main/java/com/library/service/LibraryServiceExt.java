58a6832eaacdeb03070b92063baf282b0b580fdb
Shubham Tawde <shubhamtawde.tawde@gmail.com>
2021-06-26 16:17:51 +0530

    Added Add Book method

--- src/main/java/com/library/service/LibraryServiceExt.java
+++ src/main/java/com/library/service/LibraryServiceExt.java
@@ -3,8 +3,10 @@ package com.library.service;
 import java.sql.SQLException;
 
 import com.library.constants.BookErrorConstants;
+import com.library.dto.BookInfo;
 import com.library.dto.BookResponse;
 import com.library.service.impl.LibraryServiceImpl;
+import com.library.validation.Validation;
 
 public class LibraryServiceExt 
 {
@@ -17,6 +19,56 @@ public class LibraryServiceExt
 		try
 		{
 			bookResponse = libraryServiceImpl.getBooks();
+		}
+		catch(SQLException sqlExp)
+		{
+			sqlExp.printStackTrace();
+			bookResponse.setErrorCode(errorConstants.SQL_EXP_ERROR_CODE);
+			bookResponse.setErrorMessage("SQL Exception in Class: " + getClass() + "\nCaused By: " + sqlExp.getMessage());
+			throw sqlExp;
+		}
+		catch(Exception exp)
+		{
+			exp.printStackTrace();
+			bookResponse.setErrorCode(errorConstants.GENERIC_EXP_ERROR_CODE);
+			bookResponse.setErrorMessage("SQL Exception in Class: " + getClass() + "\nCaused By: " + exp.getMessage());
+		}
+		return bookResponse;
+	}
+
+	public BookResponse addBook(BookInfo bookData) throws SQLException 
+	{
+		
+		BookResponse bookResponse = new BookResponse();
+		BookErrorConstants errorConstants = new BookErrorConstants();
+		LibraryServiceImpl libraryServiceImpl = new LibraryServiceImpl();
+		Validation validationCheck = new Validation();
+		
+		//checks for validation whether object passed is empty or not
+		if(bookData.getBookId() <= 0)
+		{
+			bookResponse.setErrorCode(-1);
+			bookResponse.setErrorMessage("Book ID Cannot be negative/zero");
+		}
+		if(bookData.getBookQty() <= 0)
+		{
+			bookResponse.setErrorCode(-1);
+			bookResponse.setErrorMessage("Book Quantity cannot be negative/zero");
+		}
+		if(!validationCheck.checkChar(bookData.getBookName()) || !validationCheck.checkChar(bookData.getBookAuthor()))
+		{
+			bookResponse.setErrorCode(errorConstants.NO_CHAR_PRESENT);
+			bookResponse.setErrorMessage("Enter Alphabets only");
+		}
+		if(bookData.getBookName().isEmpty() || bookData.getBookAuthor().isEmpty())
+		{
+			bookResponse.setErrorCode(errorConstants.IS_EMPTY_FIELD);
+			bookResponse.setErrorMessage("Book Name/Author cannot be empty!");
+		}
+		
+		try
+		{
+			bookResponse = libraryServiceImpl.addBook(bookData);
 		}
 		catch(SQLException sqlExp)
 		{

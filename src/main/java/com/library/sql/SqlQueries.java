67fe9ad53baa7753f8033be35f7fae1d78cd9773
Shubham Tawde <shubhamtawde.tawde@gmail.com>
2021-06-26 16:19:22 +0530

    Added Query for Add Book

--- src/main/java/com/library/sql/SqlQueries.java
+++ src/main/java/com/library/sql/SqlQueries.java
@@ -3,4 +3,6 @@ package com.library.sql;
 public class SqlQueries 
 {
 	public static final String GET_BOOKS = "SELECT * FROM book_info";
+	
+	public static final String ADD_BOOK = "INSERT INTO book_info" + " VALUES " + " (?, ?, ?, ?);";
 }

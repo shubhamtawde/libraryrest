f7ad9513abc3c66992bc7e1a77e52ce31feba7af
Shubham Tawde <32428188+shubhamtawde@users.noreply.github.com>
2021-07-01 21:21:31 +0530

    Added Query for Delete Book

--- src/main/java/com/library/sql/SqlQueries.java
+++ src/main/java/com/library/sql/SqlQueries.java
@@ -5,4 +5,6 @@ public class SqlQueries
 	public static final String GET_BOOKS = "SELECT * FROM book_info";
 	
 	public static final String ADD_BOOK = "INSERT INTO book_info" + " VALUES " + " (?, ?, ?, ?);";
+	
+	public static final String DELETE_BOOK = "INSERT INTO book_info" + " VALUES " + " (?, ?, ?, ?);";
 }

e1fc371c96a92479a58b64b47eb08a4b74bf6e49
Shubham Tawde <32428188+shubhamtawde@users.noreply.github.com>
2021-07-09 22:55:37 +0530

    Added main logic for Delete Book

--- src/main/java/com/library/service/impl/LibraryServiceImpl.java
+++ src/main/java/com/library/service/impl/LibraryServiceImpl.java
@@ -126,7 +126,6 @@ public class LibraryServiceImpl
 		DatabaseConnection databaseConnection = new DatabaseConnection();
 		BookResponse bookResponse = new BookResponse();
 		BookErrorConstants errorConstants = new BookErrorConstants();
-		ResultSet dbResult = null;
 		Connection dbConn = null;
 		PreparedStatement sqlStatement = null;
 		SqlQueries sqlQuery = new SqlQueries();
@@ -218,9 +217,89 @@ public class LibraryServiceImpl
 		return bookResponse;
 	}
 
-	public BookResponse deleteBook(String bookName) 
+	public BookResponse deleteBook(int bookId) throws Exception 
 	{
+		DatabaseConnection databaseConnection = new DatabaseConnection();
+		BookResponse bookResponse = new BookResponse();
+		BookErrorConstants errorConstants = new BookErrorConstants();
+		Connection dbConn = null;
+		PreparedStatement sqlStatement = null;
+		SqlQueries sqlQuery = new SqlQueries();
 		
-	}
-
+		try
+		{
+			dbConn = databaseConnection.getConnection();
+			if(dbConn != null)
+			{
+				dbConn.setAutoCommit(false);
+				sqlStatement = dbConn.prepareStatement(sqlQuery.DELETE_BOOK);
+				sqlStatement.setInt(1, bookId);
+				if(sqlStatement.executeUpdate() > 0)
+				{
+					bookResponse.setResultMessage("Deleted Book with Book ID: " + bookId + " successfully!");
+					dbConn.commit();
+				}
+				else
+				{
+					throw new SQLException();
+				}
+			}
+			else
+			{
+				throw new SQLException();
+			}
+		}
+		catch(SQLException sqlExp)
+		{
+			//rollback the connection
+			dbConn.rollback();
+			sqlExp.printStackTrace();
+			bookResponse.setErrorCode(errorConstants.SQL_EXP_ERROR_CODE);
+			bookResponse.setErrorMessage("SQL Exception in Class: " + getClass() + "\nCaused By: " + sqlExp.getMessage());
+			throw sqlExp;
+		}
+		catch(Exception exp)
+		{
+			dbConn.rollback();
+			exp.printStackTrace();
+			bookResponse.setErrorCode(errorConstants.GENERIC_EXP_ERROR_CODE);
+			bookResponse.setErrorMessage("Generic Exception in Class: " + getClass() + "\nCaused By: " + exp.getMessage());
+			throw exp;
+		}
+		
+		finally
+    	{
+    		if(sqlStatement != null)
+    		{
+    			try
+    			{
+	    			if(!sqlStatement.isClosed())
+	    				sqlStatement.close();
+    			}
+    			catch(SQLException err)
+    			{
+    				err.printStackTrace();
+    				bookResponse.setErrorCode(errorConstants.SQL_EXP_ERROR_CODE);
+    				bookResponse.setErrorMessage("SQL Exception in Class: " + getClass() + "\nCaused By: " + err.getMessage());
+    				throw err;
+    			}
+    		}
+    		if(dbConn != null)
+    		{
+    			try
+    			{
+	    			if(!dbConn.isClosed())
+	    				databaseConnection.closeConnection(dbConn);
+    			}
+    			catch(SQLException err)
+    			{
+    				err.printStackTrace();
+    				bookResponse.setErrorCode(errorConstants.SQL_EXP_ERROR_CODE);
+    				bookResponse.setErrorMessage("SQL Exception in Class: " + getClass() + "\nCaused By: " + err.getMessage());
+    				throw err;
+    			}
+    		}
+    	}
+    return bookResponse;
+    }
 }
